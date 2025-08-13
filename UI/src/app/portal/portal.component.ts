3de1fb5444f6abf8ae599aed72e34a8c2d5b6f82
szymbaramichal <szymbaramichal@gmail.com>
2025-08-13 20:12:25 +0200

    feat: Fixing angular routing, adding auth component

--- UI/src/app/portal/portal.component.ts
+++ UI/src/app/portal/portal.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthComponent } from '../auth/auth.component';
 
 @Component({
   selector: 'app-portal',
   standalone: true,
-  imports: [],
+  imports: [AuthComponent],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.css'
 })
