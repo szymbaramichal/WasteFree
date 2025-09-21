a15023f29b393fab4638cc523430c2f9db3e8248
szymbaramichal <szymbaramichal@gmail.com>
2025-09-21 13:13:14 +0200

    feat: Adding envs

--- UI/src/app/auth/auth.service.ts
+++ UI/src/app/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://dev.wastefreecloud.pl/api/auth';
+  private apiUrl = `${environment.apiUrl}/auth`;
 
   constructor(private http: HttpClient) {}
 
