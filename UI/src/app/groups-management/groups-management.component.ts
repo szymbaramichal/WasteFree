b60ecd89f8d71ab14a42bdf41e3eddb95b755bf5
szymbaramichal <szymbaramichal@gmail.com>
2025-09-28 13:00:04 +0200

    feat: Fixing responsiveness, new componens and many other (like always ehh)

---
+++ UI/src/app/groups-management/groups-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-groups-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './groups-management.component.html',
+  styleUrl: './groups-management.component.css'
+})
+export class GroupsManagementComponent {
+
+}
