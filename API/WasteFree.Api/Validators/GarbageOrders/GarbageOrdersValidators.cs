26dfe390f94bc0aca6eedf22aebe1a1b08923e4a
szymbaramichal <szymbaramichal@gmail.com>
2025-12-12 19:06:23 +0100

    fix: Update date validation to use UTC now for pickup and drop-off dates

--- API/WasteFree.Api/Validators/GarbageOrders/GarbageOrdersValidators.cs
+++ API/WasteFree.Api/Validators/GarbageOrders/GarbageOrdersValidators.cs
@@ -24,11 +24,11 @@ public class GarbageOrderRequestValidator : AbstractValidator<GarbageOrderReques
         RuleFor(x => x.PickupDate)
             .NotEmpty()
             .WithErrorCode(ValidationErrorCodes.PickupDateRequired)
-            .GreaterThanOrEqualTo(DateTime.Today)
+            .GreaterThanOrEqualTo(DateTime.UtcNow)
             .WithErrorCode(ValidationErrorCodes.PickupDateInPast);
 
         RuleFor(x => x.DropOffDate)
-            .Must(dropOff => !dropOff.HasValue || dropOff.Value >= DateTime.Today)
+            .Must(dropOff => !dropOff.HasValue || dropOff.Value >= DateTime.UtcNow)
             .WithErrorCode(ValidationErrorCodes.DropOffDateInPast);
 
         RuleFor(x => x.UserIds)
